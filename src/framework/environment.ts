8839f2e222c60719a479adb0c695369c9fd78658
Maor Ben Noach <bennoachmaor@gmail.com>
2023-09-25 03:31:46 +0300

    Moved variables to environment with docker

--- src/framework/environment.ts
+++ src/framework/environment.ts
@@ -1,7 +1,4 @@
 import env from 'env-var';
 
-// export const monitorApiUrl = env.get('uri').required().asString();
-// export const botToken = env.get('botToken').required().asString();
-
-export const monitorApiUrl = "http://localhost:9090/";
-export const botToken = "SECRET";
+export const monitorApiUrl = env.get('uri').required().asString();
+export const botToken = env.get('botToken').required().asString();
